64d40ae1d5843a6c3333f2195958578874388c97
Samah <Samahosama112@icloud.com>
2024-08-10 17:11:48 +0300

    creat app styles

--- lib/Features/home/presentaion/view/widgets/home_view_body.dart
+++ lib/Features/home/presentaion/view/widgets/home_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:booly_app/Features/home/presentaion/view/widgets/books_list_view.dart';
 import 'package:booly_app/Features/home/presentaion/view/widgets/custom_app_bar.dart';
 import 'package:booly_app/core/styles.dart';
+import 'package:booly_app/core/utils/assets_data.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -18,7 +19,47 @@ class HomeViewBody extends StatelessWidget {
           SizedBox(height: 50),
           Text(
             'Best Seller',
-            style: Styles.titleMedium,
+            style: Styles.textStyle18,
+          ),
+          BestSellerListViewItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 3 / 5,
+            child: Container(
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(15)),
+                image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(AssetsData.kTestImage),
+                ),
+              ),
+            ),
+          ),
+          Column(
+            children: [
+              Text('Book title lklfkelfkel'),
+              Text('writer name'),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('Price 19.99'),
+                  Text('rate '),
+                ],
+              )
+            ],
           )
         ],
       ),
--- lib/constant.dart
+++ lib/constant.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 const kPrimaryColor = Color(0xff100B20);
 const kTransitionDuration = Duration(seconds: 1);
-
-
+const kTitleFont1 = GoogleFonts.libreBaskervilleTextTheme;
