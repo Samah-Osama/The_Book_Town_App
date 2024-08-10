9eb83108fec6556cf5e7b4dffabf0098e05c4fae
Samah <Samahosama112@icloud.com>
2024-08-10 11:26:09 +0300

    first commit

---
+++ lib/core/utils/assets_data.dart
@@ -0,0 +1,3 @@
+class AssetsData {
+  static const logo = 'assets/Logo/Logo2.PNG';
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+
+import 'package:booly_app/Features/spalsh_feature/presentaion/views/splash_view.dart';
+import 'package:booly_app/constant.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: kPrimaryColor,
+      ),
+      home: const SplashView(),
+    );
+  }
+}
