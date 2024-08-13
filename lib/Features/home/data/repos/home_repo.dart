9ac2c91c8d79e4c4fe4016ffa4be19847b55bb02
Samah <Samahosama112@icloud.com>
2024-08-13 12:53:13 +0300

    creat home repo methodes and failure class

--- lib/Features/home/data/repos/home_repo.dart
+++ lib/Features/home/data/repos/home_repo.dart
@@ -1,4 +1,8 @@
+import 'package:booly_app/Features/home/data/models/book_model/book_model.dart';
+import 'package:booly_app/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
-  fetchGeneralBooks();
-  fetchBestSellerBooks();
+ Future<Either<Failure,List<BookModel>>> fetchGeneralBooks();
+ Future<Either<Failure,List<BookModel>>> fetchBestSellerBooks();
 }
