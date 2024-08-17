22886f5bb0704ca3ba2798fb00ad174d7119b418
Samah <Samahosama112@icloud.com>
2024-08-17 17:02:28 +0300

    creat general books method

--- lib/Features/home/presentaion/view_models/general_books_cubit/generalbooks_cubit.dart
+++ lib/Features/home/presentaion/view_models/general_books_cubit/generalbooks_cubit.dart
@@ -1,10 +1,23 @@
 import 'package:bloc/bloc.dart';
 import 'package:booly_app/Features/home/data/models/book_model/book_model.dart';
-import 'package:dartz/dartz.dart';
+import 'package:booly_app/Features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
-
 part 'generalbooks_state.dart';
 
 class GeneralbooksCubit extends Cubit<GeneralbooksState> {
-  GeneralbooksCubit() : super(GeneralbooksInitial());
+  GeneralbooksCubit(this.homeRepo) : super(GeneralbooksInitial());
+  final HomeRepo homeRepo;
+  Future<void> fetchGeneralBooks() async {
+    emit(GeneralbooksLoading());
+    var result = await homeRepo.fetchGeneralBooks();
+    result.fold((failure) {
+      emit(
+        GeneralbooksFailure(errorMessege: failure.errorMessege),
+      );
+    }, (book) {
+      emit(
+        GeneralbooksSuccess(books: book),
+      );
+    });
+  }
 }
